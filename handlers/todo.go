4c6b99ef89c303e9922316ec35ff10e7a88d7cd6
yamkant <dev.yamkim@gmail.com>
2023-10-14 21:01:46 +0900

    feat: #18 [API: todo] CRUD 관련 전반적인 수정 (detail 동작시 filter 추가)

--- handlers/todo.go
+++ handlers/todo.go
@@ -9,23 +9,22 @@ import (
 )
 
 func GetTodos(c *gin.Context) {
-	var todos []models.Todo
-
 	user := c.MustGet("user").(models.User)
 
+	var todos []models.Todo
 	models.DB.Where("user_id = ?", user.ID).Order("id desc").Find(&todos)
 
 	c.JSON(http.StatusOK, gin.H{"data": todos})
 }
 
 func AddTodo(c *gin.Context) {
+	user := c.MustGet("user").(models.User)
+
 	var bodyData models.TodoAddDTO
 	if err := c.ShouldBindJSON(&bodyData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	user := c.MustGet("user").(models.User)
 
 	todo := models.Todo{
 		Content:   bodyData.Content,
@@ -41,29 +40,33 @@ func AddTodo(c *gin.Context) {
 }
 
 func UpdateTodo(c *gin.Context) {
-	var todo models.Todo
+	user := c.MustGet("user").(models.User)
 
-	if err := models.DB.Where("id = ?", c.Param("id")).First(&todo).Error; err != nil {
+	var todo models.Todo
+	if err := models.DB.Where("id = ?", c.Param("id")).Where("user_id = ?", user.ID).First(&todo).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
 
+	// var bodyData models.TodoUpdateDTO
 	var bodyData models.TodoUpdateDTO
 	if err := c.ShouldBindJSON(&bodyData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
-	models.DB.Model(&todo).Updates(bodyData)
+	models.DB.Model(&todo).Select("*").Updates(bodyData)
+
 	c.JSON(http.StatusOK, gin.H{"data": todo})
 }
 
 func DeleteTodo(c *gin.Context) {
-	var todo models.Todo
+	user := c.MustGet("user").(models.User)
 
-	if err := models.DB.Where("id = ?", c.Param("id")).First(&todo).Error; err != nil {
+	var todo models.Todo
+	if err := models.DB.Where("id = ?", c.Param("id")).Where("user_id = ?", user.ID).First(&todo).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
-
 	models.DB.Delete(&todo)
+
 	c.JSON(http.StatusOK, gin.H{"data": true})
 }
