d15c24e91238d953d93b8e7ecee4277144f80847
yamkant <dev.yamkim@gmail.com>
2023-10-14 21:19:38 +0900

    feat: #18 [API: middleware] auth middleware 사용시 화이트 리스트 추가

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -2,27 +2,48 @@ package middlewares
 
 import (
 	"net/http"
+	"strings"
 
 	"example.com/m/models"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
+var whiteList = []string{
+	"/api/v1/users",
+}
+
+func isWhiteList(url string) bool {
+	if strings.HasPrefix(url, "/auth") {
+		return true
+	}
+	for _, v := range whiteList {
+		if v == url {
+			return true
+		}
+	}
+	return false
+}
+
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if isWhiteList(c.Request.URL.Path) {
+			c.Next()
+		}
+
 		session := sessions.Default(c)
 
 		kakaoUserID := session.Get("kakao_user_id")
 		if kakaoUserID == nil {
 			c.Redirect(http.StatusFound, "/auth/kakao")
+			c.Abort()
+			return
 		}
 
 		var user models.User
 		err := models.DB.Where("kakao_id = ?", kakaoUserID).First(&user).Error
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"error": "unauthorized",
-			})
+			c.Redirect(http.StatusFound, "/auth/kakao")
 			c.Abort()
 			return
 		}
