db127dd4fd3aa73e4f5dde378c03ef6b3e0549d4
yamkant <dev.yamkim@gmail.com>
2023-10-17 21:52:25 +0900

    refactor: #32 [Setting: google-chart] chart draw 파트 클래스로 리팩토링

--- app/views/js/todo.js
+++ app/views/js/todo.js
@@ -71,64 +71,6 @@ const onClickAddBtn = async (target) => {
     location.reload();
 }
 
-const getChartDataFormat = (completed_at, content, started_at, ended_at) => {
-    const completedDate = new Date(completed_at);
-    const startDate = new Date(started_at);
-    const endDate = new Date(ended_at);
-    return [
-        completedDate.getFullYear() + '-' + (completedDate.getMonth() + 1) + '-' + completedDate.getDate(),
-        content,
-        new Date(0,0,0,startDate.getHours(),startDate.getMinutes(),0),
-        new Date(0,0,0,endDate.getHours(),endDate.getMinutes(),0),
-    ]
-}
-
-const isValidChartType = (data) => {
-    if (!data.completed) {
-        return false;
-    }
-    if (data.started_at === '0001-01-01T00:00:00Z') {
-        return false;
-    }
-    if (data.ended_at === '0001-01-01T00:00:00Z') {
-        return false;
-    }
-    if (data.started_at >= data.ended_at) {
-        return false;
-    }
-    return true;
-}
-
-const setChart = (resData) => {
-    const myDataTable = []
-    for (const data of resData) {
-        if (!isValidChartType(data)) {
-            continue;
-        }
-        myDataTable.push(getChartDataFormat(data.completed_at, data.content, data.started_at, data.ended_at)); 
-    }
-
-    google.charts.load("current", {packages:["timeline"]});
-    google.charts.setOnLoadCallback(getHandleredDataTable);
-    function getHandleredDataTable() {
-        const container = document.getElementById('todo-chart');
-        const chart = new google.visualization.Timeline(container);
-        const options = {
-            timeline: { colorByRowLabel: true },
-            alternatingRowStyle: false
-        };
-        const dataTable = new google.visualization.DataTable();
-        dataTable.addColumn({ type: 'string', id: 'Room' });
-        dataTable.addColumn({ type: 'string', id: 'Name' });
-        dataTable.addColumn({ type: 'date', id: 'Start' });
-        dataTable.addColumn({ type: 'date', id: 'End' });
-
-        dataTable.addRows(myDataTable);
-
-        chart.draw(dataTable, options);
-    }
-
-}
 
 window.addEventListener('load', async () => {
     const todoUlContainer = document.querySelector('.todo-ul-container');
@@ -158,5 +100,6 @@ window.addEventListener('load', async () => {
         }
     }
 
-    setChart(resData);
+    const googleChartHandler = new GoogleChartHandler();
+    googleChartHandler.run(resData);
 });
